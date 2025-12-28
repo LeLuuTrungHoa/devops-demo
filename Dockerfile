0eee7120b0e6ce2dc2ee65466fa9f0dbdca94eae
LeLuuTrungHoa <hoatrung33@gmail.com>
2025-12-28 22:07:26 +0700

    Edit index & docker file

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,5 @@ COPY . .
 EXPOSE 3000
 
 # Lệnh chạy ứng dụng
-CMD ["npm", "start"]
+#CMD ["npm", "start"]
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 app.get('/', (req, res) => {
     res.send('<h1>Chào mừng đến với Demo DevOps!</h1><p>Hệ thống CI/CD đã hoạt động.</p>');
-});
-
-app.listen(PORT, () => {
-    console.log(`Ứng dụng đang chạy tại http://localhost:${PORT}`);
 });
 
 module.exports = app;
